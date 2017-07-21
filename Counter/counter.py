e5a79ad290f1d4055853552125a6b4df5caf6443
Kishan <kishan.ameerali@gmail.com>
2017-07-20 23:58:06 -0400

    Pushing all updated assignments

--- Counter/counter.py
+++ Counter/counter.py
@@ -1,7 +1,7 @@
 #Counter Assignment
 #Count the number of times someone visits the page during a session
 
-from flask import Flask,session,render_template
+from flask import Flask,session,render_template,redirect
 app = Flask(__name__)
 app.secret_key = 'secretsecret'
 
@@ -11,5 +11,10 @@ def index():
         session['counter'] = 0
     session['counter'] += 1
     return render_template('index.html')
+
+@app.route("/button", methods=['POST'])
+def plustwo():
+    session['counter'] += 2
+    redirect('/')
 
 app.run(debug=True)
