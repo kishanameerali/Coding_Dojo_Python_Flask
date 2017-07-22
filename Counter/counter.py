dbb6a84dfbdf93585193139b0a2805b07f00ebd2
Kishan <kishan.ameerali@gmail.com>
2017-07-21 22:03:12 -0400

    Thirteenth commit updated Counter assignment with extra challenges completed

--- Counter/counter.py
+++ Counter/counter.py
@@ -1,5 +1,11 @@
 #Counter Assignment
-#Count the number of times someone visits the page during a session
+'''
+Count the number of times someone visits the page during a session
+
+Extra Challenges
+-add a +2 button underneath the counter that increments the counter by 2 and reloads the page.
+-add a reset button that will reset the counter to 1
+'''
 
 from flask import Flask,session,render_template,redirect
 app = Flask(__name__)
@@ -12,9 +18,14 @@ def index():
     session['counter'] += 1
     return render_template('index.html')
 
-@app.route("/button", methods=['POST'])
+@app.route("/plustwo")
 def plustwo():
-    session['counter'] += 2
-    redirect('/')
+    session['counter'] += 1
+    return redirect('/')
+
+@app.route("/reset")
+def reset():
+    session.clear()
+    return redirect('/')
 
 app.run(debug=True)
